fed07bd1fdf4c33dbd0245482c2b5520b182e282
Arlen Dmello <arlendmello03@gmail.com>
2024-07-12 09:40:19 +0530

    axios bug fixed

--- controller/contentGetController.js
+++ controller/contentGetController.js
@@ -1,18 +1,18 @@
-// import notesModel from "../model/model4.js";
-
-// export const contentGet=async(req,res)=>{
-//     try {
-//         const notesGet=await notesModel.find()
-//         res.json(notesGet)
-//     } catch (error) {
-//         res.status(500).json({message:"Internal error"})        
-//     }
-// }
-
-
 import notesModel from "../model/model4.js";
 
 export const contentGet=async(req,res)=>{
+    try {
+        const notesGet=await notesModel.find()
+        res.json(notesGet)
+    } catch (error) {
+        res.status(500).json({message:"Internal error"})        
+    }
+}
+
+
+// import notesModel from "../model/model4.js";
+
+export const contentGet2=async(req,res)=>{
     try {
         const feid=req.params._id;
         const notesGet=await notesModel.findOne(({_id:feid}))
--- route/contentGetRoute.js
+++ route/contentGetRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { contentGet2 } from "../controller/contentGetController.js";
 import { contentGet } from "../controller/contentGetController.js";
 
 const router=express.Router()
 
-// router.get("/getContent",contentGet)
-router.patch("/ViewContent/:_id",contentGet)
+router.get("/getContent",contentGet)
+router.get("/ViewContent/:_id",contentGet2)
 
 export default router
 
